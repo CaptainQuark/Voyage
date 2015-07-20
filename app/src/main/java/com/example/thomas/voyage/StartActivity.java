034158ff473565c53d86cf7f73fff1b68e67c127
Captain Quark <a1208739@unet.univie.ac.at>
2015-07-20 17:47:04 +0200

    + Farbschema ("VoyageDark") + Ausrichtung "Landschaftsmodus" (muss für jede Activity einzeln festgelegt werden)

--- app/src/main/java/com/example/thomas/voyage/StartActivity.java
+++ app/src/main/java/com/example/thomas/voyage/StartActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class StartActivity extends Activity {
@@ -12,27 +13,10 @@ public class StartActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        View view = findViewById(R.id.activity_start_landscape);
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            // zuerst der Activity eine id geben -> danach direkt ansprechbar
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
