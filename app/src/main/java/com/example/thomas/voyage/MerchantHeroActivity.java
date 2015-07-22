33163c724ecdc4abefaed59cf6e89acd8277e54b
Captain Quark <a1208739@unet.univie.ac.at>
2015-07-22 19:11:22 +0200

    + erster Enwurf HeroMerchant

--- app/src/main/java/com/example/thomas/voyage/MerchantHeroActivity.java
+++ app/src/main/java/com/example/thomas/voyage/MerchantHeroActivity.java
@@ -2,39 +2,20 @@ package com.example.thomas.voyage;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.*;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 
-public class StartActivity extends Activity {
-
-    DBheroesAdapter heroesHelper;
-        //hier noch vorhanden -> später in passende Datei verschieben
+public class MerchantHeroActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        setContentView(R.layout.activity_merchant_hero);
 
         hideSystemUI();
-
-        //View viewLandscape = findViewById(R.id.activity_start);
-        //viewLandscape.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-
-        // zuerst der Activity eine id geben -> danach direkt ansprechbar
-        // in Manifest zur activity ".StartActivity" "android:screenOrientation="landscape"" einfügen -> immer Landschaftsmodus
-
-        /*
-        heroesHelper = new DBheroesAdapter(this);
-
-        long id = heroesHelper.insertData("Thomas", 100, "eins", "zwei");
-
-        if (id < 0) Message.message(this, "error@insert");
-        else Message.message(this, "success@insert");
-
-        String data = heroesHelper.getAllData();
-        Message.message(this, data);
-        */
     }
 
     private void hideSystemUI() {
@@ -59,8 +40,8 @@ public class StartActivity extends Activity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
-    public void clickToHeroMerchant(View view){
-        Intent i = new Intent(getApplicationContext(), MerchantHeroActivity.class);
+    public void clickToHome(View view){
+        Intent i = new Intent(getApplicationContext(), StartActivity.class);
         startActivity(i);
     }
 
