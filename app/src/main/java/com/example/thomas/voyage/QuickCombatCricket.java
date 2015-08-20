901f16e049c122ef75b3c92e45eabe07eb9e308c
Captain Quark <a1208739@unet.univie.ac.at>
2015-08-21 00:27:41 +0200

    + kv @ CricketCombat (Anzeige der Zellen)

--- app/src/main/java/com/example/thomas/voyage/QuickCombatCricket.java
+++ app/src/main/java/com/example/thomas/voyage/QuickCombatCricket.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
-import android.widget.LinearLayout;
+import android.widget.Space;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,7 +19,9 @@ import java.util.List;
 public class QuickCombatCricket extends Activity {
 
     int count = 21;
-    int tempCount = 0;
+    int[] array;
+    private static GridView cricketView;
+    private static List<Integer> markedList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +29,33 @@ public class QuickCombatCricket extends Activity {
         setContentView(R.layout.activity_quick_combat_cricket);
         hideSystemUI();
 
+        array  = new int[count];
+        for(int i = 0, temp = 0; i < count; i++){
 
-        GridView cricketView = (GridView) findViewById(R.id.cricket_gridview);
+            if(i == 20) temp = 25;
+            else temp = i+1;
+
+            array[i] = temp;
+        }
+
+        cricketView = (GridView) findViewById(R.id.cricket_gridview);
         cricketView.setAdapter(new SimpleNumberAdapter(this));
+        cricketView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                markedList.add(position);
+                cricketView.invalidateViews();
+
+            }
+        });
     }
 
-
-
+    @Override
+    protected void onRestart() {
+        super.onRestart();  // Always call the superclass method first
+        hideSystemUI();
+    }
 
 
 
@@ -62,41 +84,47 @@ public class QuickCombatCricket extends Activity {
             return 0;
         }
 
+        class ViewHolder {
+            TextView numView, playerOneView, playerTwoView;
+            Space spaceOne, spaceTwo;
+            int progressPlayerOne = 0, progressPlayerTwo = 0;
+
+            ViewHolder(View v){
+                numView = (TextView) v.findViewById(R.id.quick_cricket_card_textview);
+                playerOneView = (TextView) v.findViewById(R.id.cricketcard_progress_1);
+                playerTwoView = (TextView) v.findViewById(R.id.cricketcard_progress_2);
+                spaceOne = (Space) v.findViewById(R.id.cricketcard_space_1);
+                spaceTwo = (Space) v.findViewById(R.id.cricketcard_space_2);
+            }
+        }
+
         // create a new ImageView for each item referenced by the Adapter
         public View getView(final int position, View convertView, ViewGroup parent) {
 
-            LayoutInflater inflater = (LayoutInflater) mContext
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            ViewHolder holder = null;
 
-            View gridView;
-            final TextView tv;
+            if(convertView == null){
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.quick_cricket_card, parent, false);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
 
-
-            if (convertView == null) {
-
-                gridView = new View(mContext);
-                gridView = inflater.inflate( R.layout.quick_cricket_card , parent, false);
-
-                tv = (TextView) gridView.findViewById(R.id.quick_cricket_card_textview);
-
-            } else {
-                gridView = (View) convertView;
+            }else{
+                holder = (ViewHolder) convertView.getTag();
             }
 
+            if(markedList.contains(position)){
+                //holder.numView.setText(Integer.toString(array[position]));
+                convertView.setBackgroundColor(Color.BLACK);
 
-            gridView.setOnClickListener(new View.OnClickListener() {
+            }else{
+                convertView.setBackgroundDrawable(getResources().getDrawable(R.drawable.rounded_corners));
+            }
 
-                @Override
-                public void onClick(View v) {
+            holder.numView.setText(array[position] + "");
 
-                    //tv.setBackgroundColor(Color.BLUE);
-                    //Message.message(getApplicationContext(), "size of list: " + selectionList.size());
-                }
-            });
-
-            return gridView;
+            return convertView;
         }
-
     }
 
 
