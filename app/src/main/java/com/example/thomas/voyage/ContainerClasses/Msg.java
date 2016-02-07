9431b7594df07e49bb1dabc6af216693116d4d5c
Captain Quark <a1208739@unet.univie.ac.at>
2016-02-07 09:30:09 +0100

    + 'evasion'

--- app/src/main/java/com/example/thomas/voyage/ContainerClasses/Msg.java
+++ app/src/main/java/com/example/thomas/voyage/ContainerClasses/Msg.java
@@ -7,4 +7,8 @@ public class Msg {
     public static void msg(Context context, String message){
         Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
+
+    public static void msgShort(Context c, String msg){
+        Toast.makeText(c, msg, Toast.LENGTH_SHORT).show();
+    }
 }
